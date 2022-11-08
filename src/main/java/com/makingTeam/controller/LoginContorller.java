69f1262d5676c49028be335587116b84bcbb7253
skaru <ngy1950@kakao.com>
2022-11-08 18:27:15 +0900

    2022.11.08

--- src/main/java/com/makingTeam/controller/LoginContorller.java
+++ src/main/java/com/makingTeam/controller/LoginContorller.java
@@ -2,6 +2,8 @@ package com.makingTeam.controller;
 
 import com.makingTeam.vo.LoginVO;
 import com.makingTeam.vo.common.ApplicationOauthKeyVO;
+import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +19,7 @@ import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -33,24 +36,60 @@ public class LoginContorller {
     public ModelAndView sign(){
         System.out.println("@@@@@@ /login/sign start @@@@@@");
         ModelAndView mav = new ModelAndView("thymeleaf/login");
+        mav.addObject("loginKakao", appKeyVO.getKAKAO_LOGIN_URL());
         return mav;
     }
 
     @RequestMapping(value = "/sign/kakao")
     @ResponseBody
-    public String signKakao(@RequestParam HashMap<String,Object> param) throws IOException{
+    public void getAuthorizeKakao(@RequestParam HashMap<String,Object> param) throws IOException{
         System.out.println("@@@@@@ /sign/kakao start @@@@@@");
-//        ModelAndView mav = new ModelAndView("thymeleaf/common/apiView");
-//        mav.addObject("htmlBody",request(appKeyVO.getKAKAO_LOGIN_URL(), HttpMethodType.POST, null));
 
-        return request(appKeyVO.getKAKAO_LOGIN_URL(), HttpMethodType.POST, null);
+        if(param != null){
+            try {
+                HashMap<String, Object> map = new HashMap<String, Object>();
+                StringBuilder sb = new StringBuilder();
+
+                sb.append("grant_type=authorization_code");
+                sb.append("&client_id=" + appKeyVO.getKAKAO_REST_API_KEY());
+                sb.append("&redirect_uri=" + appKeyVO.getREDIRECT_URL_SIGN());
+                sb.append("&code=" + param.get("code").toString());
+
+                map.put("HttpMethodType", HttpMethodType.POST.toString());
+                map.put("StringBuilder", sb);
+
+                String result = request(appKeyVO.getURL(appKeyVO.getACCESSTOKEN_URL()), map);
+
+                getUserInfoKakao(stringToHashMap(result).get("access_token").toString());
+
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    public String request(String requestUrl, HttpMethodType httpMethod, StringBuilder sb) throws IOException {
+    public void getUserInfoKakao(String access_token){
+        System.out.println("@@@@@ start getUserInfoKakao @@@@@");
+
+        try {
+
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("HttpMethodType", HttpMethodType.POST.toString());
+            map.put("adminPaths", appKeyVO.getGET_USER_URL());
+            map.put("accessToken", access_token);
+            request(appKeyVO.getURL(appKeyVO.getGET_USER_URL()), map);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String request(String requestUrl, HashMap<String,Object> paramMap) throws IOException {
         String line = null;
         InputStream in = null;
         BufferedReader reader = null;
         HttpsURLConnection httpsConn = null;
+        StringBuilder sb = null;
         StringBuilder rs = new StringBuilder();
 
         try{
@@ -66,8 +105,20 @@ public class LoginContorller {
                 }
             });
 
-            if(httpMethod == null){
-                httpMethod = httpMethod.GET;
+            // HttpMethodType httpMethod
+            if(paramMap.get("HttpMethodType") == null){
+                paramMap.put("HttpMethodType", HttpMethodType.GET);
+            }
+
+            if(paramMap.get("StringBuilder") != null){
+                sb = (StringBuilder) paramMap.get("StringBuilder");
+            }
+
+            if(paramMap.get("adminPaths") != null){
+                String findPath = appKeyVO.getAdminPaths()[Arrays.binarySearch(appKeyVO.getAdminPaths(), paramMap.get("adminPaths").toString())];
+                if(findPath != null){
+                    httpsConn.setRequestProperty("Authorization", "Bearer " + paramMap.get("access_token".toString()));
+                }
             }
 
             // Input setting
@@ -75,7 +126,7 @@ public class LoginContorller {
             // Output setting
             httpsConn.setDoOutput(true);
             // Header Setting
-            httpsConn.setRequestMethod(httpMethod.toString());
+            httpsConn.setRequestMethod(paramMap.get("HttpMethodType").toString());
 
             if(sb != null){
                 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(httpsConn.getOutputStream()));
@@ -122,6 +173,10 @@ public class LoginContorller {
         return null;
     }
 
+    /**
+     * @param : HashMap
+     * @return : String
+     * */
     public String mapToParam(HashMap<String, Object> map){
         StringBuilder paramBuilder = new StringBuilder();
         for(String key : map.keySet()){
@@ -132,101 +187,13 @@ public class LoginContorller {
         return paramBuilder.toString();
     }
 
-    public void kakaoTest(String accessToken) throws IOException {
-        // "https://kauth.kakao.com/oauth/authorize?client_id=c69874219bbb8b7fe847a0e55f3dba05&redirect_uri=http://localhost:8080/login/sign/kakao&response_type=code"  // 로그인 페이지 호출
-        // https://kauth.kakao.com/oauth/token  // 엑세스 토큰 url
-        // https://kapi.kakao.com/v2/user/me    // 사용자 정보 url
-
-        String urlString = "https://kauth.kakao.com/oauth/token";
-        String line = null;
-        InputStream in = null;
-        BufferedReader reader = null;
-        HttpsURLConnection httpsConn = null;
-
-        try {
-            // Get HTTPS URL connection
-            URL url = new URL(urlString);
-            httpsConn = (HttpsURLConnection) url.openConnection();
-
-            // Set Hostname verification
-            httpsConn.setHostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    // Ignore host name verification. It always returns true.
-                    return true;
-                }
-            });
-
-            // Input setting
-            httpsConn.setDoInput(true);
-            // Output setting
-            httpsConn.setDoOutput(true);
-            // Caches setting
-//            httpsConn.setUseCaches(false);
-            // Read Timeout Setting
-//            httpsConn.setReadTimeout(1000);
-            // Connection Timeout setting
-//            httpsConn.setConnectTimeout(1000);
-            // Method Setting(GET/POST)
-            /** 카카오 최초 로그인시 */
-//            httpsConn.setRequestMethod("GET"); // kakao login
-//            httpsConn.setRequestProperty("HeaderKey","HeaderValue");
-
-            /** 카카오 로그인 후 권한 요청 시 */
-            httpsConn.setRequestMethod("POST"); // 카카오 로그인 확인 권한
-            httpsConn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(httpsConn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=authorization_code");
-            sb.append("&client_id=" + appKeyVO.getKAKAO_REST_API_KEY());
-            sb.append("&redirect_uri=http://localhost:8080/login/sign/kakao");
-            sb.append("&code=" + accessToken);
-            bw.write(sb.toString());
-            bw.flush();
-            // Header Setting
-
-
-            int responseCode = httpsConn.getResponseCode();
-            System.out.println("응답코드 : " + responseCode);
-            System.out.println("응답메세지 : " + httpsConn.getResponseMessage());
-
-            // SSL setting
-            SSLContext context = SSLContext.getInstance("TLS");
-            context.init(null, null, null); // No validation for now
-            httpsConn.setSSLSocketFactory(context.getSocketFactory());
-
-            // Connect to host
-            httpsConn.connect();
-            httpsConn.setInstanceFollowRedirects(true);
-
-            // Print response from host
-            if (responseCode == HttpsURLConnection.HTTP_OK) { // 정상 호출 200
-                in = httpsConn.getInputStream();
-            } else { // 에러 발생
-                in = httpsConn.getErrorStream();
-            }
-            reader = new BufferedReader(new InputStreamReader(in));
-            while ((line = reader.readLine()) != null) {
-                System.out.printf("%s\n", line);
-            }
-
-            reader.close();
-        } catch (UnknownHostException e) {
-            System.out.println("UnknownHostException : " + e);
-        } catch (MalformedURLException e) {
-            System.out.println(urlString + " is not a URL I understand");
-        } catch (IOException e) {
-            System.out.println("IOException :" + e);
-        } catch (Exception e) {
-            System.out.println("error : " + e);
-        } finally {
-            if (reader != null) {
-                reader.close();
-            }
-            if (httpsConn != null) {
-                httpsConn.disconnect();
-            }
-        }
+    /**
+     * @param : String
+     * @return : HashMap
+     * @설명 : 문자열을 HasMap 객체로 변환
+     * */
+    public HashMap<String, Object> stringToHashMap(String str) throws ParseException {
+        return new JSONParser(str).object();
     }
+
 }
--- src/main/java/com/makingTeam/vo/common/ApplicationOauthKeyVO.java
+++ src/main/java/com/makingTeam/vo/common/ApplicationOauthKeyVO.java
@@ -2,26 +2,29 @@ package com.makingTeam.vo.common;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class ApplicationOauthKeyVO {
     final private String KAKAO_REST_API_KEY;
     final private String KAKAO_JAVASCRIPT_KEY;
     final private String KAKAO_ADMIN_KEY;
-    final private String REDIRECT_LOGIN_URL = "http://localhost:8080/login/sign/kakao";
-    final private String KAKAO_ACCESSTOKEN_URL = "https://kauth.kakao.com/oauth/token";
-    final private String KAKAO_GET_USER_URL = "https://kapi.kakao.com/v2/user/me";
+    final private String HOST_URL = "https://kauth.kakao.com/";
+    final private String ACCESSTOKEN_URL = "oauth/token";
+    final private String GET_USER_URL = "v2/user/me";
+    final private String REDIRECT_URL_SIGN = "http://localhost:8080/login/sign/kakao";
+    private String[] adminPaths = null;
 
-    final private String REDIRECT_URI_LOGIN_SIGN_KAKAO = "http://localhost:8080/login/sign/kakao";
-//    https://kauth.kakao.com/oauth/authorize?client_id=c69874219bbb8b7fe847a0e55f3dba05&redirect_uri=http://localhost:8080/login/sign/kakao&response_type=code
     public ApplicationOauthKeyVO(){
         this.KAKAO_REST_API_KEY = "c69874219bbb8b7fe847a0e55f3dba05";
         this.KAKAO_JAVASCRIPT_KEY = "72fe965decd3024c98db2fe76e0718eb";
         this.KAKAO_ADMIN_KEY = "c95328242fa70e59fe844f7c859f5250";
+        this.adminPaths = new String[]{ACCESSTOKEN_URL, GET_USER_URL};
     }
     public String getKAKAO_LOGIN_URL(){
-        return "https://kauth.kakao.com/oauth/authorize?client_id=" + this.getKAKAO_REST_API_KEY() + "&redirect_uri=" + REDIRECT_LOGIN_URL + "&response_type=code";
+        return "https://kauth.kakao.com/oauth/authorize?client_id=" + this.getKAKAO_REST_API_KEY() + "&redirect_uri=" + REDIRECT_URL_SIGN + "&response_type=code";
     }
-    public String getKAKAO_LOGIN_URL(String redirect_uri){
-        return "https://kauth.kakao.com/oauth/authorize?client_id=" + this.getKAKAO_REST_API_KEY() + "&redirect_uri=" + redirect_uri + "&response_type=code";
+    public String getURL(String request_uri){
+        return HOST_URL + request_uri;
     }
 }
